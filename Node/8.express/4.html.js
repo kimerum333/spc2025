a2718c256b23ac26a09d4ee195d3083fec11bcb6
KSG <kimerum333@gmail.com>
2025-04-15 10:11:02 +0900

    수업 시작 전 복습

--- Node/8.express/4.html.js
+++ Node/8.express/4.html.js
@@ -10,7 +10,7 @@ const port = 3000;
 // 정적 파일 제공: /image → ./image 폴더
 app.use('/image', express.static(path.join(__dirname, 'image')));
 
-app.get('/',(req,res)=>{
+app.get('/',(req,resp)=>{
     //__dirname : 현재 이 파일이 위치한 경로를 담고 있는 전역 변수.
     const htmlFilePath = path.join(__dirname,'1.imagegallery.html'); //절대경로()
     res.sendFile(htmlFilePath);    
