7771774f58558a845144281e7ec3df4c43c0df90
KSG <kimerum333@gmail.com>
2025-04-18 16:52:40 +0900

    python/300제 하는중

---
+++ Python/0.300/to_10.py
@@ -0,0 +1,26 @@
+#Hello world
+print('Hello World')
+
+#Mary's Cosmatic
+print("Mary's Cosmetic")
+
+#
+print('신씨가 소리질렀다. "도둑이야".')
+
+print('C:\Windows')
+
+#\n 줄바꿈, \t 탭
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+
+print('naver;kakao;sk;samsung')
+
+
+#end 와 sep 등의 옵션 활용
+print("naver", "kakao", "samsung", sep=";")
+
+print('naver/kakao/sk/samsung')
+print("naver", "kakao", "samsung", sep="/")
+
+print("first",end='');print("second",end='\n')
+
+print(5/3)
